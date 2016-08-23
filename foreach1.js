ae4d33ca96d4226a182b2b1fed21dabcc41b956e
Jonathan Tan <Jonalogy@Jonathans-MacBook-Air.local>
2016-08-23 17:17:36 +0800

    Completed forEach practices

--- foreach1.js
+++ foreach1.js
@@ -2,10 +2,15 @@
 	For Each:
 	Use the .forEach iterator to loop over the following array of foods and say you like them.
  */
- 
+
 var foods = ["pizza", "tacos", "ice cream"];
 
 // your code here
+
+foods.forEach(function (shoutout){
+  console.log("I like "+ shoutout + "!");
+});
+
 
 // The output should be
 // > "I like pizza"
--- foreach2.js
+++ foreach2.js
@@ -1,13 +1,20 @@
 /*
 	For Each 2:
-	Use the .forEach iterator to loop over the following array of objects and say how delicious each one is.
+	Use the .forEach iterator to loop over the following array
+  of objects and say how delicious each one is.
  */
- 
+
  var foods = [
   {name: "Pizza", level: "very"},
   {name: "Tacos", level: "mostly"},
   {name: "Cottage Cheese", level: "not very"}
 ];
+
+foods.forEach(function(yup){
+  console.log(yup['name']+ " is " + yup.level + " delicious!")
+}
+);
+
 
 // your code here
 
