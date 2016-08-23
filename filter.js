d6cbdb306185918f7a5cab3bf144413fb8d23829
Jonathan Tan <Jonalogy@Jonathans-MacBook-Air.local>
2016-08-23 19:26:13 +0800

    Completed .filter() iterator practice

--- filter.js
+++ filter.js
@@ -19,11 +19,21 @@ var people = [
 
 // Your code here!
 
-// Write code that uses the filter function, and the below isCool function to return a list of people who are cool.
+// Write code that uses the filter function, and the below
+//isCool function to return a list of people who are cool.
 
 var isCool = function(person) {
 	return person.coolnessScore > 20;
 }
+
+var coolDudes = people.filter(isCool); // coolDudes is an array of object
+console.log(coolDudes.length+" persons are cool!");
+// console.log(coolDudes);
+var display = coolDudes.forEach(function(coolPerson){
+	return console.log(coolPerson.name + " is cool");
+});
+
+console.log(display);
 
 // Example results:
 //
