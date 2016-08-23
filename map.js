224c8ed0cc92dacb7b8fb7ee4f22a3c2472909dc
Jonathan Tan <Jonalogy@Jonathans-MacBook-Air.local>
2016-08-23 17:22:47 +0800

    Complete array .map exercise

--- map.js
+++ map.js
@@ -2,9 +2,14 @@ var people = ['Bob', 'Jen', 'James', 'Isolde'];
 
 // Your code here!
 
-// Write some code using map that takes the above array of objects, and turns the array elements to all caps
+var caps = people.map(function(name){
+  return name.toUpperCase();
+});
+
+console.log(caps);
+// Write some code using map that takes the above array
+//of objects, and turns the array elements to all caps
 
 // ex:
 // console.log(people);
 // > [ 'BOB', 'JEN', 'JAMES', 'ISOLDE' ]
-
